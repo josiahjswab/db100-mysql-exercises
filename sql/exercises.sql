f152a461c581eb6d8a001c92ce63403211d76c9d
Josiah Swab <josiahjswab@gmail.com>
2020-01-14 10:37:50 -0800

    Completes excercise.

--- sql/exercises.sql
+++ sql/exercises.sql
@@ -227,17 +227,60 @@ LEFT JOIN actor ON customer.last_name = actor.last_name;
 
 -- # 9b. Select the customer first_name/last_name and actor first_name/last_name columns from performing a right join between the customer and actor column on the last_name column in each table. (i.e. `customer.last_name = actor.last_name`)
 
-
+SELECT 
+customer.first_name, 
+customer.last_name, 
+actor.first_name,
+actor.last_name
+FROM customer
+RIGHT JOIN actor ON customer.last_name = actor.last_name;
 
 -- # 9c. Select the customer first_name/last_name and actor first_name/last_name columns from performing an inner join between the customer and actor column on the last_name column in each table. (i.e. `customer.last_name = actor.last_name`)
 
+SELECT 
+customer.first_name, 
+customer.last_name, 
+actor.first_name,
+actor.last_name
+FROM customer
+INNER JOIN actor ON customer.last_name = actor.last_name;
 
--- # 9d. Select the city name and country name columns from the city table, performing a left join with the country table to get the country name column.
+-- # 9d. Select the city name and country name columns from the city table, 
+-- performing a left join with the country table to get the country name column.
 
+SELECT 
+city.city, 
+country.country 
+FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
 
 -- # 9e. Select the title, description, release year, and language name columns from the film table, performing a left join with the language table to get the "language" column.
 -- # Label the language.name column as "language" (e.g. `select language.name as language`)
 
+SELECT
+film.title,
+film.description,
+film.release_year,
+language.name AS language
+FROM film
+LEFT JOIN language ON film.language_id = language.language_id;
 
--- # 9f. Select the first_name, last_name, address, address2, city name, district, and postal code columns from the staff table, performing 2 left joins with the address table then the city table to get the address and city related columns.
+-- # 9f. Select the first_name, last_name, address, address2, city name, district, and postal code columns from the staff table, 
+-- performing 2 left joins with the address table then the city table to get the address and city related columns.
 
+SELECT
+first_name,
+last_name,
+address,
+address2,
+district,
+postal_code,
+city
+FROM staff
+LEFT JOIN address ON staff.address_id = address.address_id
+LEFT JOIN city ON address.city_id = city.city_id;
+
+-- GET COLUMN NAMES:
+SELECT COLUMN_NAME
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = N'staff'; -- <-- This string is the table to crawl.
